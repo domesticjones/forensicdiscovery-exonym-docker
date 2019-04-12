b62b2daf148c274b9af80aa5541b1bd646be3dd2
Dustin Jones <info@domesticjones.com>
2019-04-12 12:11:59 -0600

    Pull all data into footer

--- footer.php
+++ footer.php
@@ -1,21 +1,30 @@
 			<footer id="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
 				<div class="wrap">
-					<p class="copyright">&copy; Copyright <?php ex_brand('legal'); ?></p>
-					<nav class="nav-footer" role="navigation">
-						<?php wp_nav_menu(array(
-							'container' => 'ul',                    // enter '' to remove nav container
-							'container_class' => 'footer-links cf',	// class of container (should you choose to use it)
-							'menu' => __('Footer', 'exonym'),	      // nav name
-							'menu_class' => 'nav footer-nav cf',    // adding custom nav class
-							'theme_location' => 'footer-menu',		  // where it's located in the theme
-							'before' => '',							            // before the menu
-							'after' => '',							            // after the menu
-							'link_before' => '',					          // before each link
-							'link_after' => '',						          // after each link
-							'depth' => 1,							              // limit the depth of the nav
-							'fallback_cb' => ''						          // fallback function
-						)); ?>
-					</nav>
+					<section class="footer-left">
+						<img src="<?php ex_logo('primary', 'light'); ?>" alt="Logo for <?php ex_brand(); ?>" />
+						<?php
+							ex_contact('phone', true, 'global');
+							ex_contact('email', true, 'global');
+							ex_contact('address', true, 'global');
+						?>
+						<nav class="nav-footer" role="navigation">
+							<?php wp_nav_menu(array(
+								'container' => 'ul',                    // enter '' to remove nav container
+								'menu' => __('Footer', 'exonym'),	      // nav name
+								'theme_location' => 'footer-menu',		  // where it's located in the theme
+								'before' => '',							            // before the menu
+								'after' => '',							            // after the menu
+								'link_before' => '',					          // before each link
+								'link_after' => '',						          // after each link
+								'depth' => 1,							              // limit the depth of the nav
+								'fallback_cb' => ''						          // fallback function
+							)); ?>
+							<p class="copyright">&copy; Copyright <?php ex_brand('legal'); ?></p>
+						</nav>
+					</section>
+					<section class="footer-right">
+						<?php echo do_shortcode('[contact-form-7 id="132" title="Footer Contact Form"]'); ?>
+					</section>
 				</div>
 			</footer>
 		</div>
